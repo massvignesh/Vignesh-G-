fdbdd430cfc46d3c6b8f10591077b585f6900df0
VIGNESH G <117510243+massvignesh@users.noreply.github.com>
2023-06-22 14:27:48 +0530

    Add files via upload

---
+++ jothi.py
@@ -0,0 +1,38 @@
+print("Fibonacci series")
+Num_Of_Terms = int(input("Enter the number of series :"))
+f1 = -1
+f2 = 1
+
+
+for i in range(0,Num_Of_Terms,1):
+    terms = f1 + f2
+    print(terms,end=" ")
+    f1 = f2
+    f2 = terms
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
